5c8946a581328c073188c36f305dabd7d3e29e2d
MDFLAMorANGELS <valentinmd78@gmail.com>
2023-09-13 19:38:23 +0200

    footer v1 ajout du template

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,13 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [
+    require('flowbite/plugin'),
     VitePluginSass({
       // Options de configurations pour sass a mettre ici
     }),
