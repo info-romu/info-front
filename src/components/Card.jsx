af8deeb09183062d09f3a4cc4040314bc7499e81
caroline <gensac.caroline@gmail.com>
2023-09-18 17:37:04 +0200

    ajout image url produits

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
+
 const Card = ({ item }) => {
+
   return (
     <div className="card">
       <div className="card_body">
--- src/pages/MarketPlace.jsx
+++ src/pages/MarketPlace.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
+import imagesMarketPlace from '../data-images/marketplace.json'
 
 
 export default function MarketPlace() {
+  const imgData  = imagesMarketPlace.marketplace;
   const [items, setItems] = useState([]);
 
 useEffect(() => {
