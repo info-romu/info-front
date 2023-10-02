7b92a7aa725e2409c32d75ad9862f1a12fa779b1
rochaya <olivier.g77580@gmail.com>
2023-10-02 03:53:28 +0200

    ajout success et cancel checkout

--- src/pages/Cancel.jsx
+++ src/pages/Cancel.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Cancel() {
   return (
-    <div>Cancel</div>
+    <div className='container-success'>
+      <div class="smooth-top rounded-lg w-80 text-center font-bold justify-center mx-auto bg-white-500 my-5 shadow-2xl">
+        <h2>Paiement annulé ⛔.</h2>
+        <p class="mx-auto w-full text-black">
+          Réessayer quand vous voulez !
+        </p>
+      </div>
+
+      <div class="mt-5 flex justify-center space-x-4">
+        <div class="flex justify-center gap-5">
+          <Link to="/profile/:id" className="bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">Mon espace.</Link>
+          <Link to="/" className="bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">Aller a l'acceuil</Link>
+        </div>
+      </div>
+    </div>
   )
 }
