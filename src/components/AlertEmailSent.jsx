deacb1002c3d7817f2cae2aaf176279182b1e92a
caroline <gensac.caroline@gmail.com>
2023-10-02 12:41:28 +0200

    correction alert email sent

--- src/components/AlertEmailSent.jsx
+++ src/components/AlertEmailSent.jsx
@@ -21,7 +21,7 @@ const AlertEmailSent = ({ showAlert, setShowAlert }) => {
   return (
     showAlert && (
       <div
-        className={`p-4 mb-4 mx-auto mt-16 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 w-1/4 z-50 m-4 transition-opacity ease-in-out duration-300 ${
+        className={`p-4 mx-auto text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 w-1/4 m-4 transition-opacity ease-in-out duration-300 ${
           isHiding ? "opacity-0" : ""
         }`}
         role="alert"
